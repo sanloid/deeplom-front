b210dcf024c15f693527235001b9630f5a9f5fdc
Alexandr <sanloid2@gamil.com>
2023-03-25 03:50:56 +0600

    clean test page

--- src/pages/TestPage.tsx
+++ src/pages/TestPage.tsx
@@ -1,19 +1,5 @@
-import React, { useState } from "react";
-import { observer } from "mobx-react-lite";
-import AddressForm from "../forms/AddressForm";
-import { Modal } from "../components/UI/Modal";
+import React from "react";
 
-export const TestPage: React.FC = observer(() => {
-  const [visible, setVisible] = useState(false);
-  return (
-    <div>
-      <button onClick={() => setVisible(true)}>qwe</button>
-      <Modal
-        visible={visible}
-        setVisible={setVisible}
-        children={AddressForm}
-        name={"Address"}
-      />
-    </div>
-  );
-});
+export const TestPage: React.FC = () => {
+  return <></>;
+};
